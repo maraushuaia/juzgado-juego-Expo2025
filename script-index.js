4423823a21b01089d9c7d0a7da3f7458d661e0f3
Rodrigo MUÑOZ <maraushuaia@gmail.com>
2025-09-25 17:48:50 -0300

    Actualizacion de inicio y cambio de estilos

--- script-index.js
+++ script-index.js
@@ -1,3 +1,3 @@
-document.getElementById("start-btn").addEventListener("click", () => {
+document.getElementById("start-img-btn").addEventListener("click", function () {
   window.location.href = "questions.html";
 });
